34b7af8ba5946b7e5a1b03671ff2f7e68aad5600
Sergiusz <codeimperium@gmail.com>
2017-11-30 19:24:57 +0100

    Test url request

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("hell")
+import urllib.request
+import json
+
+with urllib.request.urlopen('http://scop2.mrc-lmb.cam.ac.uk/graph/restapi/chart?term=SF:3000098') as response:
+   html = json.loads(response.read())
+
+# print(html)
+
+for line in html:
+    print(line)
